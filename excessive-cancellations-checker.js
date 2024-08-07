3bb1dc58bf5de44b85b7bd1cca44d58b39794067
Naryshkin, Ilia <ilia_naryshkin@epam.com>
2024-08-07 22:14:13 +0300

    plan algorithm

--- excessive-cancellations-checker.js
+++ excessive-cancellations-checker.js
@@ -1,27 +1,47 @@
 export class ExcessiveCancellationsChecker {
+  /**
+   * We provide a path to a file when initiating the class
+   * you have to use it in your methods to solve the task
+   * @param {string} filePath
+   */
+  constructor(filePath) {
+    this.filePath = filePath;
+  }
 
-    /* 
-        We provide a path to a file when initiating the class
-        you have to use it in your methods to solve the task
-    */
-    constructor(filePath) {
-        this.filePath = filePath;
-    }
+  /**
+   * Returns the list of companies that are involved in excessive cancelling.
+   * If, in any given 60 second period and for a given company,
+   * the ratio of the cumulative quantity of cancels to cumulative quantity of orders is greater than `1/3`
+   * then the company is engaged in excessive cancelling.
+   * @returns {Promise<Array<string>>}
+   */
+  async companiesInvolvedInExcessiveCancellations() {
+    const excessiveCancellingCompanies = new Set();
 
-    /**
-    * Returns the list of companies that are involved in excessive cancelling.
-    * Note this should always resolve an array or throw error.
-    */
-    async companiesInvolvedInExcessiveCancellations() {
-        //TODO Implement...
-    }
+    const companiesTransactions = new Map();
 
-    /**
-     * Returns the total number of companies that are not involved in any excessive cancelling.
-     * Note this should always resolve a number or throw error.
-    */
-    async totalNumberOfWellBehavedCompanies() {
-        //TODO Implement...
-    }
-    
+    // TODO: Stream read the file and process it line by line
+
+    // TODO: Validate each line
+
+    // TODO: Define a order transaction object - { company: string, timestamp: number, total: number, cancelled: number }
+
+    // TODO: Handle transactions on timestamp passed 60 seconds
+
+    // TODO: Handle every transaction left transaction after end of file, via separate for loop
+
+    // TODO: Ignore companies that is already in the excessiveCancellingCompanies set
+
+    return Array.from(excessiveCancellingCompanies);
+  }
+
+  /**
+   * Returns the total number of companies that are not involved in any excessive cancelling.
+   * @returns {Promise<number>}
+   */
+  async totalNumberOfWellBehavedCompanies() {
+    const nonExcessiveCancellingCompaniesCount = 0;
+
+    return nonExcessiveCancellingCompaniesCount;
+  }
 }
